2b95d63f9ec00c38d02442001da64adb13f8a688
nooooooname <2992865700@qq.com>
2018-11-22 16:56:52 +0800

    检测回文序列

--- mirrorp.lisp
+++ mirrorp.lisp
@@ -1,9 +1,10 @@
 (defun mirror? (s)
-  "判断一个列表是不是回文列表，若s不是cons对象则返回nil"
-  (and (consp s)
+  "判断一个序列是不是回文序列，
+  若s是序列且回文返回T，否则返回nil。"
+  (and (or (consp s) (vectorp s))
        (let ((len (length s)))
 	 (let ((mid (if (evenp len)
 		      (/ len 2)
 		      (/ (- len 1) 2))))
 	   (equal (subseq s 0 mid)
-		(reverse (subseq s (- len mid))))))))
+		  (reverse (subseq s (- len mid))))))))
